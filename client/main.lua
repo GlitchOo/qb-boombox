c94e79078b8ce30082dcb6f332919d4bff337742
GlitchOo <melissa.g.misfit@gmail.com>
2021-10-31 03:08:59 -0400

    upload
    This is the start of this project

---
+++ client/main.lua
@@ -0,0 +1,210 @@
+-- Variables
+
+local QBCore = exports['qb-core']:GetCoreObject()
+local PlayerData = {}
+local currentData = nil
+
+-- Functions
+
+local function loadAnimDict(dict)
+  while (not HasAnimDictLoaded(dict)) do
+      RequestAnimDict(dict)
+      Wait(5)
+  end
+end
+
+local function helpText(text)
+	SetTextComponentFormat("STRING")
+    AddTextComponentString(text)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+-- Handlers
+
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    PlayerData = QBCore.Functions.GetPlayerData()
+end)
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerData.job = JobInfo
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    PlayerData = {}
+end)
+
+
+-- Events
+
+RegisterNetEvent('qb-boombox:client:placeBoombox', function()
+    loadAnimDict("anim@heists@money_grab@briefcase")
+    TaskPlayAnim(PlayerPedId(), "anim@heists@money_grab@briefcase", "put_down_case", 8.0, -8.0, -1, 1, 0, false, false, false)
+    Citizen.Wait(1000)
+    ClearPedTasks(PlayerPedId())
+    local coords = GetEntityCoords(PlayerPedId())
+    local heading = GetEntityHeading(PlayerPedId())
+    local forward = GetEntityForwardVector(PlayerPedId())
+    local x, y, z = table.unpack(coords + forward * 0.5)
+    local object = CreateObject(GetHashKey('prop_boombox_01'), x, y, z, true, false, false)
+    PlaceObjectOnGroundProperly(object)
+    SetEntityHeading(object, heading)
+    FreezeEntityPosition(object, true)
+    currentData = NetworkGetNetworkIdFromEntity(object)
+end)
+
+RegisterNetEvent('qb-boombox:client:pickupBoombox', function()
+    local obj = NetworkGetEntityFromNetworkId(currentData)
+    local objCoords = GetEntityCoords()
+    NetworkRequestControlOfEntity(obj)
+    loadAnimDict("anim@heists@narcotics@trash")
+    TaskPlayAnim(PlayerPedId(), "anim@heists@narcotics@trash", "pickup", 8.0, -8.0, -1, 1, 0, false, false, false)
+    Citizen.Wait(700)
+    SetEntityAsMissionEntity(obj,false,true)
+    DeleteEntity(obj)
+    DeleteObject(obj)
+    if not DoesEntityExist(obj) then
+        TriggerServerEvent('qb-boombox:server:pickedup', currentData)
+        TriggerServerEvent('QBCore:Server:AddItem', 'boombox', 1)
+        currentData = nil
+    end
+    Citizen.Wait(500)
+    ClearPedTasks(PlayerPedId())
+end)
+
+RegisterNetEvent('qb-boombox:client:playMusic', function()
+    local musicMenu = {
+      {
+          isHeader = true,
+          header = '💿 | Boombox'
+      },
+      {
+          header = '🎶 | Play a song',
+          txt = 'Enter a youtube URL',
+          params = {
+              event = 'qb-boombox:client:musicMenu',
+              args = {
+
+              }
+          }
+      },
+      {
+          header = '⏸️ | Pause Music',
+          txt = 'Pause currently playing music',
+          params = {
+              isServer = true,
+              event = 'qb-boombox:server:pauseMusic',
+              args = {
+                  entity = currentData,
+              }
+          }
+      },
+      {
+          header = '▶️ | Resume Music',
+          txt = 'Resume playing paused music',
+          params = {
+              isServer = true,
+              event = 'qb-boombox:server:resumeMusic',
+              args = {
+                  entity = currentData,
+              }
+          }
+      },
+      {
+          header = '🔈 | Change Volume',
+          txt = 'Resume playing paused music',
+          params = {
+              event = 'qb-boombox:client:changeVolume',
+              args = {
+
+              }
+          }
+      },
+      {
+          header = '❌ | Turn off music',
+          txt = 'Stop the music & choose a new song',
+          isServer = true,
+          params = {
+              isServer = true,
+              event = 'qb-boombox:server:stopMusic',
+              args = {
+                  entity = currentData,
+              }
+          }
+      },
+      {
+          header = '❌ | Pickup',
+          txt = 'Stop the music & choose a new song',
+          params = {
+              event = 'qb-boombox:client:pickupBoombox',
+              args = {
+              }
+          }
+      }
+    }
+    exports['qb-menu']:openMenu(musicMenu)
+end)
+
+RegisterNetEvent('qb-boombox:client:musicMenu', function()
+    local dialog = exports['qb-input']:ShowInput({
+        header = 'Song Selection',
+        submitText = "Submit",
+        inputs = {
+            {
+                type = 'text',
+                isRequired = true,
+                name = 'song',
+                text = 'YouTube URL'
+            }
+        }
+    })
+    if dialog then
+        if not dialog.song then return end
+        TriggerServerEvent('qb-boombox:server:playMusic', dialog.song, currentData, GetEntityCoords(NetworkGetEntityFromNetworkId(currentData)))
+    end
+end)
+
+RegisterNetEvent('qb-boombox:client:changeVolume', function()
+    local dialog = exports['qb-input']:ShowInput({
+        header = 'Music Volume',
+        submitText = "Submit",
+        inputs = {
+            {
+                type = 'text', -- number doesn't accept decimals??
+                isRequired = true,
+                name = 'volume',
+                text = 'Min: 0.01 - Max: 1'
+            }
+        }
+    })
+    if dialog then
+        if not dialog.volume then return end
+        TriggerServerEvent('qb-boombox:server:changeVolume', dialog.volume, currentData)
+    end
+end)
+
+CreateThread(function()
+    while true do
+        local sleep = 1000
+        if LocalPlayer.state['isLoggedIn'] then
+            local ped = PlayerPedId()
+            local coords    = GetEntityCoords(ped)
+            local object = GetClosestObjectOfType(coords, 3.0, GetHashKey('prop_boombox_01'), false, false, false)
+            if DoesEntityExist(object) then
+                local objCoords = GetEntityCoords(object)
+                if #(coords - objCoords) < 4 then
+                  currentData = NetworkGetNetworkIdFromEntity(object)
+                    helpText('Press ~INPUT_CONTEXT~ to play music.')
+                    if IsControlJustReleased(0, 38) then
+                        TriggerEvent('qb-boombox:client:playMusic')
+                    end
+                    sleep = 5
+                else
+                  exports['qb-menu']:closeMenu()
+                end
+            else
+              exports['qb-menu']:closeMenu()
+            end
+        end
+        Wait(sleep)
+    end
+end)
---
+++ server/main.lua
@@ -0,0 +1,46 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local xSound = exports.xsound
+local isPlaying = false
+
+QBCore.Functions.CreateUseableItem("boombox", function(source, item)
+	local src = source
+	local Player = QBCore.Functions.GetPlayer(src)
+	Player.Functions.RemoveItem('boombox', 1, false)
+	
+	TriggerClientEvent('qb-boombox:client:placeBoombox', src)
+	TriggerClientEvent('QBCore:Notify', src, 'You have just dropped the BoomBox', 'info')
+end)
+
+RegisterNetEvent('qb-boombox:server:playMusic', function(song, entity, coords)
+    local src = source
+    xSound:PlayUrlPos(-1, tostring(entity), song, Config.DefaultVolume, coords)
+    xSound:Distance(-1, tostring(entity), Config.radius)
+    isPlaying = true
+end)
+
+RegisterNetEvent('qb-boombox:server:pickedup', function(entity)
+    local src = source
+    xSound:Destroy(-1, tostring(entity))
+end)
+
+RegisterNetEvent('qb-boombox:server:stopMusic', function(data)
+    local src = source
+    xSound:Destroy(-1, tostring(data.entity))
+    TriggerClientEvent('qb-boombox:client:playMusic', src)
+end)
+
+RegisterNetEvent('qb-boombox:server:pauseMusic', function(data)
+    local src = source
+    xSound:Pause(-1, tostring(data.entity))
+end)
+
+RegisterNetEvent('qb-boombox:server:resumeMusic', function(data)
+    local src = source
+    xSound:Resume(-1, tostring(data.entity))
+end)
+
+RegisterNetEvent('qb-boombox:server:changeVolume', function(volume, entity)
+    local src = source
+    if not tonumber(volume) then return end
+    xSound:setVolume(-1, tostring(entity), volume)
+end)
